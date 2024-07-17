8500750afda1d0dafa472672a6613753c3fd5af8
Николай Вигдоров <vigdorov@MacBook-Air-Nikolaj.local>
2024-07-17 23:06:27 +0300

    add css minify

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
 
 module.exports = {
     mode: 'development',
@@ -15,6 +16,7 @@ module.exports = {
         splitChunks: {
             chunks: 'all',
         },
+        minimizer: [new CssMinimizerPlugin()],
     },
     devServer: {
         static: './dist',
